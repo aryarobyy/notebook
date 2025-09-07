b7e319f18b6e92fc246ffa7aac8799bf37d5c853
aryarobyy <robyaryanata12345@gmail.com>
2025-09-07 17:50:44 +0700

    Model folder management, and add new model

---
+++ lib/models/subtask/subtask_model.dart
@@ -0,0 +1,20 @@
+import '../date_time.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'subtask_model.freezed.dart';
+part 'subtask_model.g.dart';
+
+@freezed
+sealed class SubTaskModel with _$SubTaskModel {
+  factory SubTaskModel({
+    required String id,
+    required String text,
+    required List<String> noteId,
+    @JsonKey(fromJson: DateTimeFromJson) required DateTime createdAt,
+    @JsonKey(fromJson: DateTimeFromJsonNullable) DateTime? lastActive,
+    @Default(false) bool isDone,
+  }) = _SubTaskModel;
+
+  factory SubTaskModel.fromJson(Map<String, dynamic> json) =>
+      _$SubTaskModelFromJson(json);
+}
