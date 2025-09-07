768d4b5cdf4b7898de77026ead37c3b9fb26cb65
aryarobyy <robyaryanata12345@gmail.com>
2025-09-07 17:51:50 +0700

    Model folder management, and add new model

--- lib/models/index.dart
+++ lib/models/index.dart
@@ -1,3 +1,5 @@
-export 'user_model.dart';
-export 'note_model.dart';
-export 'category_model.dart';
+export 'user/user_model.dart';
+export 'note/note_model.dart';
+export 'category/category_model.dart';
+export 'todo/todo_model.dart';
+export 'subtask/subtask_model.dart';
