fb809f5ef49b56a417297694139e21a62a8d1ba9
aryarobyy <robyaryanata12345@gmail.com>
2025-10-19 15:26:23 +0700

    (+) Add todo in dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,5 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:to_do_list/component/widget/card/todo_card.dart';
+import 'package:to_do_list/models/index.dart';
+import 'package:to_do_list/notifiers/todo_notifier.dart';
 import 'package:to_do_list/notifiers/user_notifier.dart';
 import 'package:to_do_list/pages/calendar/calendar.dart';
 import 'package:to_do_list/pages/focus/focus.dart';
@@ -14,6 +19,8 @@ class Dashboard extends ConsumerStatefulWidget {
   @override
   ConsumerState<Dashboard> createState() => _DashboardState();
 }
+
+final TextEditingController _todoTitleController = TextEditingController();
 
 class _DashboardState extends ConsumerState<Dashboard> {
   int _currIndex = 0;
@@ -42,10 +49,23 @@ class _DashboardState extends ConsumerState<Dashboard> {
     await notifier.currentUser();
   }
 
+  void _postTodo(String userId, String title) async {
+    final notifier = ref.watch(todoNotifierProvider.notifier);
+    if (title.isEmpty) {
+      print("Judul gak boleh kosong");
+    }
+
+    await notifier
+        .createTodo(
+        creatorId: userId,
+        title: title,
+    );
+    Navigator.pop(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     final state    = ref.watch(userNotifierProvider);
-    final notifier = ref.read(userNotifierProvider.notifier);
     final userState = state.user;
 
     if (userState == null) {
@@ -65,12 +85,13 @@ class _DashboardState extends ConsumerState<Dashboard> {
       body: widgetOption[_currIndex],
       floatingActionButton: userState != null
           ? FloatingActionButton(
-        onPressed: () {
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(
-              builder: (_) => AddNote(userId: userState.id),
-            ),
+        onPressed: () async {
+          await showDialog(
+            context: context,
+            builder: (context) => _buildAddTodo(
+              context,
+              userState.id,
+            )
           );
         },
         backgroundColor: Theme.of(context).colorScheme.primary,
@@ -95,6 +116,112 @@ class _DashboardState extends ConsumerState<Dashboard> {
         splashColor: Theme.of(context).colorScheme.secondary.withOpacity(0.3),
         splashRadius: 20,
       ),
+    );
+  }
+
+  Widget _buildAddTodo(BuildContext context, String userId) {
+    return Stack(
+      children: [
+        BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: 6, sigmaY: 6),
+          child: Container(
+            color: Colors.white.withOpacity(0.4),
+          ),
+        ),
+        Positioned(
+          top: 0,
+          left: 0,
+          child: IconButton(
+            icon: const Icon(
+              Icons.close,
+              size: 40,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            padding: EdgeInsets.all(8),
+            constraints: BoxConstraints(),
+          ),
+        ),
+        Center(
+          child: Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              constraints: BoxConstraints(
+                maxWidth: 600,
+                minWidth: 300,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Card(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    elevation: 3,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(8),
+                            child: Image.asset(
+                              "assets/images/google.png",
+                              width: 48,
+                              height: 48,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          const SizedBox(width: 12),
+
+                          Expanded(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                TextField(
+                                  controller: _todoTitleController,
+                                  autofocus: true,
+                                  style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                  decoration: InputDecoration(
+                                    hintText: "Masukkan judul",
+                                    border: InputBorder.none,
+                                    isDense: true,
+                                    contentPadding: EdgeInsets.zero,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          // IconButton(
+                          //   icon: const Icon(
+                          //     Icons.star,
+                          //     color: Colors.amber,
+                          //   ),
+                          //   onPressed: () {},
+                          // ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: ElevatedButton(
+                      onPressed: () => _postTodo(userId, _todoTitleController.text),
+                      child: const Text('Simpan'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+        ),
+      ],
     );
   }
 }
